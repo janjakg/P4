64ba1da9c3645b3dc79db3ec1987eb1140bf7a8e
janjakg <janjakg@yahoo.fr>
2019-09-13 10:26:54 +0200

    initial commit

---
+++ posts.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mer. 11 sep. 2019 à 19:40
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `blogp4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ view/frontend/listPostsView.php
@@ -0,0 +1,30 @@
+<?php $title = "Billet simple pour l'Alaska"; ?>
+
+<?php ob_start(); ?>
+<h1>Billet simple pour l'Alaska</h1>
+<p>Derniers posts :</p>
+
+
+<?php
+while ($data = $posts->fetch())
+{
+?>
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($data['title']) ?>
+            <em>le <?= $data['creation_date_fr'] ?></em>
+        </h3>
+        
+        <p>
+            <?= nl2br(htmlspecialchars($data['content'])) ?>
+            <br />
+            <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em>
+        </p>
+    </div>
+<?php
+}
+$posts->closeCursor();
+?>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
