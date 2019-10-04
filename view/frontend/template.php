b9b340c30376c337490a2ed7e454a15d59848361
janjakg <janjakg@yahoo.fr>
2019-10-04 10:27:21 +0200

    ajout barre de navigation

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -10,9 +10,29 @@
   </head>
 
   <body>
+  
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">Accueil</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">      
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?action=post&id=1">Posts</a>
+      </li>           
+    </ul>
+    <ul class="navbar-nav ml-auto">      
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?action=listPosts">Connexion</a>
+      </li>           
+    </ul>    
+  </div>
+</nav>
     <div class="container">
       <?= $content ?>
     </div>
     <script src="public/js/signal.js"></script>
   </body>
-</html>
+</html>       
