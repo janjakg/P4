596021c8cb31785ff4854e8a748b339eafe09162
janjakg <janjakg@yahoo.fr>
2019-10-10 21:27:57 +0200

    ajout tableau pour creer la liste des commentaires

--- view/backend/listComments.php
+++ view/backend/listComments.php
@@ -7,7 +7,34 @@
 </div>
 
 <section class="shadow-lg p-3 mb-5 bg-white rounded">
-    <p>Liste des commentaires</p>
+    <div class="container"><br>
+      <div class="row"><br>
+        <h2>Liste des Posts :</h2>  
+      </div>
+      <div class="row">
+          <div class="table-responsive"><br>
+            <table class="table table-hover table-bordered"><br>
+              <thead>
+                <tr>                
+                  <th scope="col">Id</th>
+                  <th scope="col">Comment</th>                
+                  <th scope="col">Signalement</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                  <tr>                
+                    <?php while ($row = $listComment->fetch()): ?>                
+                      <td><?= nl2br(htmlspecialchars($row['comment'])) ?></td>
+                      <td><?= nl2br(htmlspecialchars($row['signalled'])) ?></td>
+                      <td></td>  
+                    <?php endwhile; ?>                                
+                  </tr>                                    
+                </tr>
+              </tbody>
+            </table>
+          </div>
+      </div><br> 
 </section>
 
 <?php $content = ob_get_clean(); ?>
