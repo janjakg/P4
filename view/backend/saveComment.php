8ac40676d3d896afa13d1b087d7c233d562a3191
janjakg <janjakg@yahoo.fr>
2019-10-15 16:09:23 +0200

    création page commentaire sauvegardé

---
+++ view/backend/saveComment.php
@@ -0,0 +1,16 @@
+  
+<?php $title = "Commentaire sauvegardé" ?>
+
+<?php ob_start(); ?>
+<div class="headline">
+    <h1>Billet simple pour l'Alaska</h1>
+</div>
+
+<section class="shadow-lg p-3 mb-5 bg-white rounded">
+    <p>Ce commentaire est sauvegardé</p>
+    <a href="index.php?action=adminIndex">Retour aux commentaires signalés</a>
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
