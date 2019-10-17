8fa3716d3849b35c7b2e2a4b7447b17c4943ed22
janjakg <janjakg@yahoo.fr>
2019-10-17 17:25:42 +0200

    création navbar pour l'admin

--- view/backend/adminIndex.php
+++ view/backend/adminIndex.php
@@ -5,12 +5,23 @@
 <div class="headline">
     <h1>Billet simple pour l'Alaska</h1>
 </div>
-<p>
-<a href="index.php?action=adminCrud">Liste de tous les posts</a>
-</p>
 
 <section class="shadow-lg p-3 mb-5 bg-white rounded">
     <div class="container"><br>
+
+    <nav>
+      <div class="nav nav-tabs" id="nav-tab" role="tablist">
+        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="index.php?action=adminIndex" role="tab" aria-controls="nav-home" aria-selected="true">Liste des commentaires signalés</a>
+        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="index.php?action=adminCrud" role="tab" aria-controls="nav-profile" aria-selected="false">Liste des posts</a>
+        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="index.php?action=createPost" role="tab" aria-controls="nav-contact" aria-selected="false">Créer un post</a>
+      </div>
+    </nav>
+    <div class="tab-content" id="nav-tabContent">
+      <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
+      <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+      <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+    </div>
+
       <div class="row"><br>
         <h2>Liste des commentaires signalés :</h2>  
       </div>
