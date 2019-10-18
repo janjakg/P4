aaf56447702b87d5338f3d47428fbbaffbf5212b
janjakg <janjakg@yahoo.fr>
2019-10-18 15:16:18 +0200

    réajustement de l'heure dans le modèle

--- model/PostManager.php
+++ model/PostManager.php
@@ -6,7 +6,7 @@ class PostManager extends Manager
   public function getPosts()
   {
     $db = $this-> dbconnect();
-    $req = $db-> query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+    $req = $db-> query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
     
     return $req;
   }
@@ -14,7 +14,7 @@ class PostManager extends Manager
   public function getPost($postId)
   {
     $db = $this-> dbConnect();
-    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y\') AS creation_date_fr FROM posts WHERE id = ?');
     $req->execute(array($postId));
     $post = $req->fetch();
 
@@ -24,7 +24,7 @@ class PostManager extends Manager
   public function addPost()
   {
     $db = $this->dbconnect();
-    $req = $db->prepare('INSERT INTO posts(id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr) VALUES (?, ? , ?, NOW())');
+    $req = $db->prepare('INSERT INTO posts(id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y\') AS creation_date_fr) VALUES (?, ? , ?, NOW())');
     $postAdded = $req;
    
     return $postAdded;
@@ -42,7 +42,7 @@ class PostManager extends Manager
   public function editPost($postId)
   {
     $db = $this->dbconnect();
-    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+    $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y\') AS creation_date_fr FROM posts WHERE id = ?');
     $req->execute(array($postId));
     $displayPost = $req->fetch();
 
