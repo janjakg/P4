cf49214fde81a3bc96cfd8cc24a80ed965b83915
janjakg <janjakg@yahoo.fr>
2019-10-24 10:34:32 +0200

    création page suite à la création d'un post

---
+++ view/backend/createInfo.php
@@ -0,0 +1,15 @@
+<?php $title = "Un post a été créé" ?>
+
+<?php ob_start(); ?>
+<div class="headline">
+    <h1>Billet simple pour l'Alaska</h1>
+</div>
+
+<section class="shadow-lg p-3 mb-5 bg-white rounded">
+    <p>Un post a bien été créé. Merci</p>
+    <a href="index.php?action=createPost">Retour à l'écriture de post</a>
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>
