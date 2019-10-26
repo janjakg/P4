00c726374048102ba4303d9e0a0f966bb14d875d
janjakg <janjakg@yahoo.fr>
2019-10-26 22:37:49 +0200

    changement de page dde redirection

--- view/backend/adminRegistration.php
+++ view/backend/adminRegistration.php
@@ -3,9 +3,11 @@
 
 <?php ob_start(); ?>
 
+<br>
+<br>
 <div align="center">
     <h1>INSCRIPTION</h1></div>
-    <form action="index.php?action=adminRegistration" method="post">
+    <form action="index.php?action=adminLogin" method="post">
       <p>
         <fieldset>          
           <div class="form-group row">
@@ -22,13 +24,13 @@
           </div>
           <div class="form-group row">
               <label for="email2" class="col-sm-2 col-form-label">Confirmer email :</label><br>
-              <div class="col-sm-10">
+              <div class="col-sm-10">              
                 <input type="email" class="form-control" name="email2" id="email2" placeholder="Confirmez votre email"/><br>
               </div>
           </div>
-          <div class="form-group row">
+          <div class="form-group row">          
               <label for="password" class="col-sm-2 col-form-label">Mot de passe :</label><br>
-              <div class="col-sm-10">
+              <div class="col-sm-10">              
               <input type="password" class="form-control" name="password" id="password" placeholder="Votre mot de passe" /><br><br>
               </div>
           </div>
@@ -46,4 +48,4 @@
 
 <?php $content = ob_get_clean(); ?>
 
-<?php require('template.php'); ?>
+<?php require('adminTemplate.php'); ?>
