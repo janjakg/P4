185bb770208c16fdf9b33dc280d65fc6a188422d
janjakg <janjakg@yahoo.fr>
2019-10-26 22:39:43 +0200

    création de la fonction login

--- model/RegistrationManager.php
+++ model/RegistrationManager.php
@@ -5,10 +5,20 @@ class RegistrationManager extends Manager
 {
   public function register($pseudo, $email, $password)
   {
-    $db = $this->dbconnect();
-    $req = $db->prepare('INSERT INTO members(pseudo, email, password) VALUES(?, ?, ?)');
+    $db = $this->dbconnect();   
+    $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
+    $req = $db->prepare("INSERT INTO members(pseudo, email, password) VALUES('$pseudo', '$email', '$password')");
     $registration = $req->execute([$pseudo, $email, $password]);
 
     return $registration;
+  }
+
+  public function login()
+  {
+    $db = $this->dbconnect(); 
+    $req = $db->prepare('SELECT * FROM members WHERE password = ?');
+    $log = $req;
+
+    return $log;
   }
 }
