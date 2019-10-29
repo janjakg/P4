eae3a948efdd9f228962245511b6a047f914649d
janjakg <janjakg@yahoo.fr>
2019-10-30 00:58:34 +0100

    correction methode login

--- model/RegistrationManager.php
+++ model/RegistrationManager.php
@@ -6,8 +6,7 @@ class RegistrationManager extends Manager
   public function register($pseudo, $email, $password)
   {
     $db = $this->dbconnect();   
-    $password = password_hash($_POST['password'],PASSWORD_DEFAULT);
-    $req = $db->prepare("INSERT INTO members(pseudo, email, password) VALUES('$pseudo', '$email', '$password')");
+    $req = $db->prepare("INSERT INTO member(pseudo, email, password) VALUES('$pseudo', '$email', '$password')");
     $registration = $req->execute([$pseudo, $email, $password]);
 
     return $registration;
@@ -16,8 +15,8 @@ class RegistrationManager extends Manager
   public function login()
   {
     $db = $this->dbconnect(); 
-    $req = $db->prepare('SELECT * FROM members WHERE password = ?');
-    $log = $req;
+    $req = $db->prepare("SELECT email, password FROM member WHERE email = '?'");
+    $log = $req->execute();
 
     return $log;
   }
