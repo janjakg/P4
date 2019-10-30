1f5e138afbd786d186ab4a786cdaceed149fc403
janjakg <janjakg@yahoo.fr>
2019-10-30 01:00:13 +0100

    revue du code

--- controller/frontend.php
+++ controller/frontend.php
@@ -14,25 +14,16 @@ function post()
     $commentManager = new CommentManager();
     $post = $postManager->getPost($_GET['id']);
     $comments = $commentManager->getComments($_GET['id']);
-<<<<<<< HEAD
-=======
-
->>>>>>> ecaeca1a0ebe1323be5322d10c6a84039eabd4a8
     require('view/frontend/postView.php');
 }
 function addComment($postId, $author, $comment)
 {
     $commentManager = new CommentManager();
     $affectedLines = $commentManager->postComment($postId, $author, $comment);
-<<<<<<< HEAD
-    if ($affectedLines === false) {
-       throw new Exception('Impossible d\'ajouter le commentaire !');
-=======
 
     if ($affectedLines === false) {
        throw new Exception('Impossible d\'ajouter le commentaire !');
 
->>>>>>> ecaeca1a0ebe1323be5322d10c6a84039eabd4a8
     }
     else {
         header('Location: index.php?action=post&id=' . $postId);
@@ -42,10 +33,6 @@ function signalledComment($commentId)
 {
     $commentManager = new CommentManager();
     $updateComment = $commentManager->signalledComments($commentId);
-<<<<<<< HEAD
-=======
-
->>>>>>> ecaeca1a0ebe1323be5322d10c6a84039eabd4a8
     if($updateComment === false) {
       throw new Exception('Problème dans la mise à jour');
     }
