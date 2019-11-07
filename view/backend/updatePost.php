f120f655e6fb95543859f90bce975d14f0b3668c
janjakg <janjakg@yahoo.fr>
2019-11-07 22:31:24 +0100

    ajout tinymce sur la page updatePost

--- view/backend/updatePost.php
+++ view/backend/updatePost.php
@@ -1,18 +1,29 @@
-
 <?php $title = "Post à modifier" ?>
 
 <?php ob_start(); ?>
 <div class="headline">
-    <h1>Billet simple pour l'Alaska</h1>
+  <h1>Billet simple pour l'Alaska</h1>
 </div>
 
 <section class="shadow-lg p-3 mb-5 bg-white rounded">
-    <h2>Post à modifier</h2>
-      <form action="index.php?action=adminCrud&amp;id=" method="post">
-        <textarea name="update" id="update" cols="30" rows="10"></textarea>
-        <button type="submit">Modifier</button>
-      </form> 
+  <h2>Post à modifier</h2>
+  <a href="index.php?action=adminCrud">Retour à la liste des posts</a>
+
+  <form action="index.php?action=postUpdated.php" method="post">
+    <div class="form-group">
+      <textarea name="update" id="update" cols="30" rows="10"><?=$_GET['content'] ?></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Modifier</button>
+  </form>
 </section>
+
+<script src="../../node_modules/tinymce/tinymce.min.js"></script>
+<script>
+  tinymce.init({
+    mode: 'textareas',
+    language: 'fr_FR'
+  });
+</script>
 
 <?php $content = ob_get_clean(); ?>
 
