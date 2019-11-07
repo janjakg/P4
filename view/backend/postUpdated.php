51b659ee833757f39572cb773b001328d9803fbc
janjakg <janjakg@yahoo.fr>
2019-11-07 22:32:31 +0100

    creation de la page postUpdated

---
+++ view/backend/postUpdated.php
@@ -0,0 +1,15 @@
+<?php $title = "Post modifié" ?>
+
+<?php ob_start(); ?>
+<div class="headline">
+    <h1>Billet simple pour l'Alaska</h1>
+</div>
+
+<section class="shadow-lg p-3 mb-5 bg-white rounded">
+    <p>Ce post a bien été supprimé. Merci</p>
+    <a href="index.php?action=adminCrud">Retour à la liste des posts</a>
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('adminTemplate.php'); ?>
