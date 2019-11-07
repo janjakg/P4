31f6e015a62036606cd2b5a4274bf19191e0933d
janjakg <janjakg@yahoo.fr>
2019-11-07 22:39:30 +0100

    reindentation du code

--- model/Manager.php
+++ model/Manager.php
@@ -4,6 +4,7 @@ class Manager
     protected function dbConnect()
     {
         $db = new PDO('mysql:host=localhost;dbname=blogP4;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-    return $db;
+
+        return $db;
   }
 }
