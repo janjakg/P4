d7a11b1f273f6383c2a6ad93a5a37dec22628ca9
janjakg <janjakg@yahoo.fr>
2019-11-11 17:19:32 +0100

    correction du model pour envoi des posts

--- model/RegistrationManager.php
+++ model/RegistrationManager.php
@@ -14,14 +14,14 @@ class RegistrationManager extends Manager
     return $registration;
   }
 
-  public function login()
+  /*public function login()
   {
     $db = $this->dbconnect(); 
     $req = $db->prepare("SELECT email, password FROM member WHERE email= '?'");
     $log = $req->execute();
 
     return $log;
-  }
+  }*/
 
   public function logout()
   {
