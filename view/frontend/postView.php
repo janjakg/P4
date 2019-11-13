4b6fc99522acea2cf2cf9fd191c3f469ba835abe
janjakg <janjakg@yahoo.fr>
2019-11-13 22:59:35 +0100

    creation de marges

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -3,7 +3,7 @@
 
 <?php ob_start(); ?>
 <div class="headline">
-<h1>Billet simple pour l'Alaska</h1>
+<div class="shadow-none m-5 pb-5 bg-light"><h1>Billet simple pour l'Alaska</h1></div>
 </div>
 
 
