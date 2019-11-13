ec50a0ac14842bd13006cba76171ecd7aba34acd
janjakg <janjakg@yahoo.fr>
2019-11-13 23:01:29 +0100

    creation de marges

--- view/backend/postUpdated.php
+++ view/backend/postUpdated.php
@@ -1,13 +1,13 @@
 <?php $title = "Post modifié" ?>
 
 <?php ob_start(); ?>
-<div class="headline">
-    <h1>Billet simple pour l'Alaska</h1>
+<div class="shadow-none m-5 pb-5 bg-light">
+  <h1>Billet simple pour l'Alaska</h1>
 </div>
 
 <section class="shadow-lg p-3 mb-5 bg-white rounded">
-    <p>Ce post a bien été supprimé. Merci</p>
-    <a href="index.php?action=adminCrud">Retour à la liste des posts</a>
+  <p>Ce post a bien été supprimé. Merci</p>
+  <a href="index.php?action=adminCrud">Retour à la liste des posts</a>
 </section>
 
 <?php $content = ob_get_clean(); ?>
