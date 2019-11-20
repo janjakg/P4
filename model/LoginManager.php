2a8bc8fbe1424e4d849f9e34166b3056c8eb4d45
janjakg <janjakg@yahoo.fr>
2019-11-20 21:56:26 +0100

    allègement methode login dans le model

--- model/LoginManager.php
+++ model/LoginManager.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 
 require_once('model/Manager.php');
 
@@ -8,26 +7,31 @@ class LoginManager extends Manager
   public function login($email,$password)
   {
       $db = $this->dbconnect(); 
-      $req = $db->prepare("SELECT * FROM member WHERE email= ?");
+      $req = $db->prepare("SELECT * FROM member WHERE email= ?");      
       $req->execute([$email]);
-      $member = $req->fetch();   
-      $isPasswordCorrect = password_verify($_POST['password'], $member['password']);
+      $member = $req->fetch();  
+      $password = password_verify($_POST['password'], $member['password']); 
+      //$isPasswordCorrect = password_verify($_POST['password'], $member['password']);
+
+      return $member;
     
-    if(!$member)
+    /*if(!$member)
       {
         echo 'Mauvais identifiant ou mot de passe';
         } else
         {
-          if($isPasswordCorrect)
-          {
-       
+          if($isPasswordCorrect) 
+          { 
+          //session_start();
+          $_SESSION['pseudo'] = $member['pseudo'];     
           $_SESSION['email'] = $email;
-          echo 'Vous êtes connecté !';
-          getSignaledComments();      
+          echo 'Bonjour '. $_SESSION['pseudo'] . ' vous êtes connecté !';
+         getSignaledComments();  
+          exit();
             } else 
               {
-              echo 'Mauvais identifiant ou mot de passe !';
+              echo 'Mauvais identifiant ou mot de passe !';             
               }
-        }
+        }*/
   }
 }
