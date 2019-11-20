8523459aa81bfc28c9f29d476db7edccc0df7c69
janjakg <janjakg@yahoo.fr>
2019-11-20 21:53:04 +0100

    creation page logout

--- view/backend/adminLogout.php
+++ view/backend/adminLogout.php
@@ -1,7 +1,20 @@
-<?php
-session_start();
-$_SESSION = array();
-session_destroy();
-header('Location : adminLogin.php');
-?>
+<?php $title = "Déconnexion" ?>
+
+<?php ob_start(); ?>
+<div class="shadow-none m-5 pb-5 bg-light">
+  <h1>Billet simple pour l'Alaska</h1>
+</div>
+
+<section class="shadow-lg p-3 mb-5 bg-white rounded">
+  <p>Vous êtes maintenat déconnecté !</p>
+  <a href="index.php?action=adminLogin">CONNEXION</a>
+</section>
+<section class="shadow-lg p-3 mb-5 bg-white rounded">  
+  <a href="index.php?action=listPosts">HOMEPAGE</a>
+</section>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('adminTemplate.php'); ?>
+
 
