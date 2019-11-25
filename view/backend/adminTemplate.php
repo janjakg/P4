c3cee5e754c3c860f7d7c6001268136082d2189e
janjakg <janjakg@yahoo.fr>
2019-11-25 21:01:18 +0100

    ajout de google font

--- view/backend/adminTemplate.php
+++ view/backend/adminTemplate.php
@@ -10,9 +10,10 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/fa6a9c271b.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
 </head>
 
-<body>
+<body class="background">
 
   <div class="sticky-top">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -47,9 +48,11 @@
     </nav>
   </div>
   <div class="container">
-  <img src="public/images/town" class="img-fluid" alt="Responsive image">
+  <div class="text-center">
+    <img src="public/images/town" alt="town"></div>
+ 
     <?= $content ?>
-  </div>
+  </div> 
   <footer>
   Site réalisé par Jean-Jacques dans le cadre de la formation OpenClassrooms. 2019 
   </footer>
