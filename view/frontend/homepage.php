574fa7dc6c522853e130004876ec1bc1f53316df
janjakg <janjakg@yahoo.fr>
2019-11-27 11:14:25 +0100

    css pour nouvel onglet homepage

---
+++ view/frontend/homepage.php
@@ -0,0 +1,70 @@
+<?php $title = "Accueil" ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?= $title ?></title>
+  <link href="public/css/style.css" rel="stylesheet" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/fa6a9c271b.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
+</head>
+
+<body class="background2">
+
+  <div class="sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+
+      <?php if (isset($_SESSION['pseudo'])) :?>
+      <span class="badge badge-pill badge-success">connecté </span>
+      <?php else :?>
+      <p> </p>
+      <?php endif;?>
+
+      <a class="navbar-brand" href="index.php"><strong><em>Jean FORTEROCHE</em></strong></a>
+      <a class="navbar-brand" href="index.php"><i class="fas fa-home"></i> Accueil</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <!-- l'élément li suivant sera utile si on veut mettre en place l'élément INSCRIPTION -->
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?action=adminRegistration"></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?action=homepage"> Homepage</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"> Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?action=adminLogin"><i class="fas fa-user-lock"></i> Admin</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+
+  <div class="container-fluid">
+   <h1 class="paysage">A propos de JEAN FORTEROCHE</h1>
+   <p><img src="public/images/writer" alt="photo_ecrivain"></p>
+<p></p>  
+  
+  </div>
+
+  <script src="public/js/signal.js"></script>
+</body>
+
+<footer>
+    Site réalisé par Jean-Jacques GOMIS dans le cadre de la formation OpenClassrooms. 2019
+  </footer>
+  
+
+</html>
