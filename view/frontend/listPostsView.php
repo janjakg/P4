bdd095d9f47838744820dcceeeff8ccd0fc79b8b
janjakg <janjakg@yahoo.fr>
2019-11-28 13:29:49 +0100

    reformatage

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -6,9 +6,12 @@ $title = "Billet simple pour l'Alaska";
 <?php ob_start();?>
 
 <div class="banner">
-  
-  <div class="titre"><h1 class=" m-5 pb-5">Billet simple pour l'Alaska</h1></p></div>
-  <img src="public/images/boreal" class="img-fluid" alt="Responsive image">
+
+  <div class="titre">
+    <h1 class=" m-5 pb-5">Billet simple pour l'Alaska</h1>
+    </p>
+  </div>
+  <div class="text-center"><img src="public/images/alaska7" class="img-fluid" alt="Responsive image"></div>
 </div>
 
 <?php while ($data = $posts->fetch()):?>
@@ -16,7 +19,8 @@ $title = "Billet simple pour l'Alaska";
 <div class="news">
   <div class="shadow p-3 mb-5 bg-white rounded">
     <h3>
-      <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>"><?= strip_tags(stripslashes($data['title'])) ?></a></em>
+      <em><a
+          href="index.php?action=post&amp;id=<?= $data['id'] ?>"><?= strip_tags(stripslashes($data['title'])) ?></a></em>
     </h3>
 
     <p><?= nl2br(strip_tags(stripslashes(substr($data['content'],0,200)))) ?>...</p>
