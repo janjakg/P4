272d6f6f1bad55321c3c0addb50981cfc1b99761
janjakg <janjakg@yahoo.fr>
2019-11-28 13:25:31 +0100

    reformatage page

--- view/frontend/signalledComment.php
+++ view/frontend/signalledComment.php
@@ -1,16 +1,17 @@
- 
-<?php $title = "Commentaire signalé" ?>
+ <?php $title = "Commentaire signalé" ?>
 
-<?php ob_start(); ?>
-<div class="headline">
-<div class="shadow-none m-5 pb-5 bg-light"><h1>Billet simple pour l'Alaska</h1></div>
-</div>
+ <?php ob_start(); ?>
+ <div class="headline">
+   <div class="shadow-none m-5 pb-5 bg-light">
+     <h1>Billet simple pour l'Alaska</h1>
+   </div>
+ </div>
 
-<section class="shadow-lg p-3 mb-5 bg-white rounded">
-    <p>Le commentaire a bien été signalé. Merci</p>
-    <a href="index.php?action=post&amp;id=<?= $_GET['idPost']?>">Retour à l'article</a>
-</section>
+ <section class="shadow-lg p-3 mb-5 bg-white rounded">
+   <p>Le commentaire a bien été signalé. Merci</p>
+   <a href="index.php?action=post&amp;id=<?= $_GET['idPost']?>">Retour à l'article</a>
+ </section>
 
-<?php $content = ob_get_clean(); ?>
+ <?php $content = ob_get_clean(); ?>
 
-<?php require('template.php'); ?>
+ <?php require('template.php'); ?>
