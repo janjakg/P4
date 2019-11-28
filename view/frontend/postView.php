4d03a859c3d8c224c2bfd3b4d750a3e9df9062b0
janjakg <janjakg@yahoo.fr>
2019-11-28 13:29:20 +0100

    reformatage de la page

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -1,58 +1,61 @@
-
 <?php $title = htmlspecialchars($post['title']); ?>
 
 <?php ob_start(); ?>
 
-<div class="shadow-none m-5 pb-5 bg-light"><div class="titre2"><h1 class="text-center">Billet simple pour l'Alaska</h1></div></div>
-<div class="text-center m-5 pb-5"><img src="public/images/alaska3" class="img-fluid" alt="Responsive image"></div>
+<div class="shadow-none m-5 pb-5 bg-azure">
+  <div class="titre2">
+    <h1 class="text-center">Billet simple pour l'Alaska</h1>
+  </div>
+</div>
+
 <section class="news">
 
-    <div class="shadow-lg p-3 mb-5 bg-white rounded">
+  <div class="shadow-lg p-3 mb-5 bg-white rounded">
 
-        <h2><?= strip_tags(stripslashes($post['title'])) ?></h2>
-        <p>
-            <?= strip_tags(stripslashes($post['content'])) ?>
-        </p>
-        <div id="date">
-            <em>le <?= $post['creation_date_fr'] ?></em>
-        </div>
+    <h2><?= strip_tags(stripslashes($post['title'])) ?></h2>
+    <p>
+      <?= strip_tags(stripslashes($post['content'])) ?>
+    </p>
+    <div id="date">
+      <em>le <?= $post['creation_date_fr'] ?></em>
     </div>
-    </section>
+  </div>
+</section>
 <section class="shadow-lg p-3 mb-5 bg-white rounded">
-    <h2>Commentaires</h2>
-    <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
-        <div class="form-group">
-            <label for="author">Auteur</label>
-            <input type="text" class="form-control" id="author" name="author"/>
-        </div>
-        <div class="form-group">
-            <label for="comment">Commentaire</label>
-            <textarea class="form-control" id="comment" name="comment"></textarea>
-        </div>
-        <div>
-            <button type="submit" class="btn btn-primary">Envoi</button>
-        </div>
-    </form>
+  <h2>Commentaires</h2>
+  <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+    <div class="form-group">
+      <label for="author">Auteur</label>
+      <input type="text" class="form-control" id="author" name="author" />
+    </div>
+    <div class="form-group">
+      <label for="comment">Commentaire</label>
+      <textarea class="form-control" id="comment" name="comment"></textarea>
+    </div>
+    <div>
+      <button type="submit" class="btn btn-primary">Envoi</button>
+    </div>
+  </form>
 </section>
 <?php if ($comments): ?>
-<div class="shadow-lg p-3 mb-5 bg-white rounded">
-<div class="comments">
-        <?php foreach ($comments as $comment): ?>
-            <div class="card mb-4">
-                <div class="card-body">
-                    <p><strong><?= htmlspecialchars($comment['author']) ?></strong>
-                        le <?= $comment['comment_date_fr'] ?></p>
-                    <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
-                    <?php if ($comment['signalled'] == 0): ?>
-                        <a href="index.php?action=signalledComment&amp;idComment=<?= $comment['id'] ?>&amp;idPost=<?= $_GET['id'] ?>"
-                           class="btn btn-warning btn-sm active" role="button" aria-pressed="true">Signaler</a>
-                    <?php else: ?>
-                        <button class="btn btn-secondary"><em>commentaire signalé</em></button>                      
-                    <?php endif; ?>
-                </div>
-            </div>
-        <?php endforeach; ?>
+<div class="shadow-lg p-3 mb-5 bg-azure rounded">
+  <div class="comments">
+    <?php foreach ($comments as $comment): ?>
+    <div class="card mb-4">
+      <div class="card-body">
+        <p><strong><?= htmlspecialchars($comment['author']) ?></strong>
+          le <?= $comment['comment_date_fr'] ?></p>
+        <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
+        <?php if ($comment['signalled'] == 0): ?>
+        <a href="index.php?action=signalledComment&amp;idComment=<?= $comment['id'] ?>&amp;idPost=<?= $_GET['id'] ?>"
+          class="btn btn-warning btn-sm active" role="button" aria-pressed="true">Signaler</a>
+        <?php else: ?>
+        <button class="btn btn-secondary"><em>commentaire signalé</em></button>
+        <?php endif; ?>
+      </div>
     </div>
+    <?php endforeach; ?>
+  </div>
 </div>
 <?php else: ?>
 <p>Pas de commentaire</p>
