c4767bc99993684153a5cef9506ac59446b40159
janjakg <janjakg@yahoo.fr>
2019-11-29 11:57:46 +0100

    ajout  script pour responsive

--- view/frontend/homepage.php
+++ view/frontend/homepage.php
@@ -59,11 +59,13 @@
       <div class="welcomeText">
         <p class="text-center"><strong>Bienvenue sur le site officiel du nouveau livre de Jean FORTEROCHE.</strong></p>
       </div>
-      <div class="text-center">
-        <a href="index.php?action=about" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Jean
-          Forteroche</a>
-        <a href="index.php?action=listPosts" class="btn btn-secondary btn-lg active" role="button"
-          aria-pressed="true">Derniers chapitres</a>
+      <div class="access">
+        <div class="text-center">
+          <a href="index.php?action=about" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Jean
+            Forteroche</a>
+          <a href="index.php?action=listPosts" class="btn btn-secondary btn-lg active" role="button"
+            aria-pressed="true">Derniers chapitres</a>
+        </div>
       </div>
     </div>
   </div>
@@ -75,6 +77,15 @@
   </footer>
 
   <script src="public/js/signal.js"></script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+  </script>
 </body>
 
 
