7e667e41cf1ebb54e2de55d171b1fb1ef4c3f2fc
janjakg <janjakg@yahoo.fr>
2019-11-29 17:30:36 +0100

    suppression élément inutile

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -9,7 +9,6 @@ $title = "Billet simple pour l'Alaska";
 
   <div class="titre">
     <h1 class=" m-5 pb-5">Billet simple pour l'Alaska</h1>
-    </p>
   </div>
   <div class="text-center"><img src="public/images/alaska7" class="img-fluid" alt="Responsive image"></div>
 </div>
@@ -18,10 +17,12 @@ $title = "Billet simple pour l'Alaska";
 
 <div class="news">
   <div class="shadow p-3 mb-5 bg-white rounded">
+
     <h3>
       <em><a
           href="index.php?action=post&amp;id=<?= $data['id'] ?>"><?= strip_tags(stripslashes($data['title'])) ?></a></em>
     </h3>
+
 
     <p><?= nl2br(strip_tags(stripslashes(substr($data['content'],0,200)))) ?>...</p>
     <p>le <?= nl2br(strip_tags(stripslashes($data['creation_date_fr']))) ?></p>
