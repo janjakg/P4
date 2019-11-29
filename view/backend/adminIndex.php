9fa4c780df2a61985185f79bc4409ede7e41047e
janjakg <janjakg@yahoo.fr>
2019-11-29 17:31:12 +0100

    suppression d'éléments inutiles

--- view/backend/adminIndex.php
+++ view/backend/adminIndex.php
@@ -9,7 +9,7 @@
 </div>
 
 <section class="shadow-lg p-3 mb-5 bg-white rounded">
-  <div class="container"><br>
+  <div class="container">
 
     <nav>
       <div class="nav nav-tabs" id="nav-tab" role="tablist">
@@ -27,24 +27,21 @@
       <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
     </div>
 
-    <div class="row"><br>
+    <div class="row">
       <h2>Liste des commentaires signalés :</h2>
     </div>
     <div class="row">
-      <div class="table-responsive-md"><br>
-        <table class="table table-hover table-bordered"><br>
+      <div class="table-responsive-md">
+        <table class="table table-hover table-bordered">
           <thead>
             <tr>
-
               <th scope="col">Comment</th>
-
               <th colspan="2" scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
-            <?php while ($row = $signaledComments->fetch()): ?>
+            <?php while ($row = $signaledComments->fetch()):?>
             <tr>
-
               <td><?= nl2br(htmlspecialchars($row['comment'])) ?></td>
               <td><a class="btn btn-danger"
                   href="index.php?action=eraseComment&amp;idComment=<?= ($row['id']) ?>">Supprimer</a></td>
@@ -57,7 +54,7 @@
           </tbody>
         </table>
       </div>
-    </div><br>
+    </div>
 </section>
 
 <?php $content = ob_get_clean(); ?>
