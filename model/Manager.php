8d39f22fda3b91ac942db79b26d0b01ec786ac00
janjakg <janjakg@yahoo.fr>
2019-12-09 11:50:53 +0100

    remise  de PDO pour WAMP

--- model/Manager.php
+++ model/Manager.php
@@ -3,7 +3,7 @@ class Manager
 {
     protected function dbConnect()
     {
-        $db = new PDO('mysql:host=localhost;dbname=blogP4;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+      $db = new PDO('mysql:host=localhost;dbname=blogP4;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
         return $db;
   }
